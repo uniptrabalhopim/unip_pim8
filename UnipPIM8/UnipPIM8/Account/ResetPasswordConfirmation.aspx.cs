d1a958b59c9e40c7012f77eb5c7bba46fb85dc6a
Fábio Damas <fabio.damas@gmail.com>
2017-11-14 00:04:07 -0200

    Retirando arquivos e código desnecessário

--- UnipPIM8/UnipPIM8/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace UnipPIM8.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- UnipPIM8/UnipPIM8/Startup.cs
+++ UnipPIM8/UnipPIM8/Startup.cs
@@ -6,7 +6,7 @@ namespace UnipPIM8
 {
     public partial class Startup {
         public void Configuration(IAppBuilder app) {
-            ConfigureAuth(app);
+            
         }
     }
 }
